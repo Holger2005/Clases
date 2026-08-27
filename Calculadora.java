b58a0c60965fc5f8c800bbaea916ae2e01b5db1b
Holger2005 <ordonezholger43@gmail.com>
2026-08-26 23:44:50 -0500

    feat: creaçion funcion promediar

--- Calculadora.java
+++ Calculadora.java
@@ -16,4 +16,10 @@ public class Calculadora {
 		return resultado;
 	}
 	
+	public double promediar(double valor1, double valor2, double valor3)
+	{
+		double resultado;
+		resultado = (valor1 + valor2 + valor3)/3;
+		return resultado;
+	}
 }
