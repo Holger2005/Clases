739efc73539a221a6150b3fcc041ee8e95da30d2
Holger2005 <ordonezholger43@gmail.com>
2026-08-26 23:43:47 -0500

    feat: creacion funcion dividir

--- Calculadora.java
+++ Calculadora.java
@@ -9,4 +9,11 @@ public class Calculadora {
 		return resultado;
 	}
 	
+	public double dividir(double valor1, double valor2)
+	{
+		double resultado;
+		resultado = valor1 / valor2;
+		return resultado;
+	}
+	
 }
