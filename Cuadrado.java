9946ec006de4bc3e1c34421d129f84f676cca4b9
Holger2005 <ordonezholger43@gmail.com>
2026-08-31 17:12:26 -0500

    feat: creación métodos calcularArea y calcularPerimetro

--- Cuadrado.java
+++ Cuadrado.java
@@ -1,5 +1,21 @@
 package com.krakedev;
 
 public class Cuadrado {
+	
+	public double lado;
+	
+	public double calcularArea() {
+		
+		double area = lado*lado;
+		
+		return area;
+	}
+	
+	public double calcularPerimetro() {
+		
+		double perimetro = lado*4;
+		
+		return perimetro;
+	}
 
 }
