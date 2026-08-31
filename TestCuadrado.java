a6efb7d11f28352afb5fdbebecbc16ad1297542b
Holger2005 <ordonezholger43@gmail.com>
2026-08-31 17:21:18 -0500

    feat: instanciar tres cuadrados con los métodos calcularArea y calcularPerimetro

--- TestCuadrado.java
+++ TestCuadrado.java
@@ -4,7 +4,47 @@ public class TestCuadrado {
 
 	public static void main(String[] args) {
 		
-
+		//Cuadrado uno
+		
+		 Cuadrado r1 = new Cuadrado();
+		 
+		 r1.lado = 3;
+		 double area;
+		 double perimetro;
+		 
+		 area = r1.calcularArea();
+		 System.out.println("Area del cuadrado uno es: " + area);
+		 
+		 perimetro = r1.calcularPerimetro();
+		 System.out.println("Perimetro del cuadrado uno es: " + perimetro);
+		 
+		//Cuadrado dos
+			
+		 Cuadrado r2 = new Cuadrado();
+		 
+		 r2.lado = 4;
+		 double area2;
+		 double perimetro2;
+		 
+		 area2 = r2.calcularArea();
+		 System.out.println("Area del cuadrado dos es: " + area2);
+		 
+		 perimetro2 = r2.calcularPerimetro();
+		 System.out.println("Perimetro del cuadrado dos es: " + perimetro2);
+		 
+		//Cuadrado tres
+			
+		 Cuadrado r3 = new Cuadrado();
+		 
+		 r3.lado = 6;
+		 double area3;
+		 double perimetro3;
+		 
+		 area3 = r3.calcularArea();
+		 System.out.println("Area del cuadrado tres es: " + area3);
+		 
+		 perimetro3 = r3.calcularPerimetro();
+		 System.out.println("Perimetro del cuadrado tres es: " + perimetro3);
 		 
 	}
 
