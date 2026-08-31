4b894e5ca14e273ba365f5130f222915290ca7a7
Holger2005 <ordonezholger43@gmail.com>
2026-08-31 15:36:40 -0500

    feat: creación del metodo calcularPerimetro

--- Rectangulo.java
+++ Rectangulo.java
@@ -1,6 +1,15 @@
 package com.krakedev;
 
 public class Rectangulo {
-
 	
+	public double base;
+	public double altura;
+
+	public double calcularPerimetro() {
+		
+		double calculo = (base*2) + (altura*2);
+		
+		return calculo;
+		
+	}
 }
